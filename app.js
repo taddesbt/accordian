5b408dab23483e0ea6876213c0d602f7643d6141
taddesbt <taddesbt@gmail.com>
2022-09-25 12:26:58 -0400

    added event listners for each panal

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 
 const panels = document.querySelectorAll('.panel');
 //console.log(accordion);
+
+panels.forEach(function (ele) {
+    //   console.log(ele);
+      ele.addEventListener('click', toggleEle);
+    })
+    
+
+function toggleEle(e) {
+        //   console.log(e.target.parentElement);
+         
+          panels.forEach(function (elem) {
+            if (e.target.parentElement === elem) {
+              elem.classList.toggle('active');
+            }
+            else {
+              elem.classList.remove('active');
+            }
+          })
+        }
